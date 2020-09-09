8ec00f5e8430ab53543d7e4d2280def313aba9b8
Yaya143 <kongsreya143@gmail.com>
2020-09-09 21:04:32 +0700

    upload project interfaces + features

--- resources/views/Backend/manage-promotion/index.blade.php
+++ resources/views/Backend/manage-promotion/index.blade.php
@@ -6,6 +6,7 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
+                             <h1>Promotion List</h1>
                             <div class="card-content collapse show">
                                 <div class="card-body card-dashboard">
                                     <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
@@ -19,18 +20,23 @@
                                         </div>
                                         <div class="row">
                                             <div class="col-sm-12">
-                                                <table class="table table-striped table-bordered dom-jQuery-events dataTable" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
+                                                <table id="datatable" class="table table-striped table-bordered dom-jQuery-events dataTable" role="grid" aria-describedby="DataTables_Table_0_info">
                                                     <thead>
-                                                    <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending" style="width: 158.6px;">Name</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Position: activate to sort column ascending" style="width: 248.2px;">Position</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Office: activate to sort column ascending" style="width: 119.4px;">Office</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Age: activate to sort column ascending" style="width: 45px;">Age</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Start date: activate to sort column ascending" style="width: 101.8px;">Start date</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Salary: activate to sort column ascending" style="width: 98px;">Salary</th></tr>
+                                                    <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending" style="width: 158.6px;">Product ID</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Position: activate to sort column ascending" style="width: 248.2px;">Product Name</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Office: activate to sort column ascending" style="width: 119.4px;">Price</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Age: activate to sort column ascending" style="width: 45px;">Discount</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Start date: activate to sort column ascending" style="width: 101.8px;">Category</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Salary: activate to sort column ascending" style="width: 10000px;">Action</th></tr>
                                                     </thead>
                                                     <tbody>
                                                     <tr role="row" class="odd">
-                                                        <td class="sorting_1">Airi Satou</td>
-                                                        <td>Accountant</td>
-                                                        <td>Tokyo</td>
-                                                        <td>33</td>
-                                                        <td>2008/11/28</td>
-                                                        <td>$162,700</td>
+                                                        <td class="sorting_1">P01</td>
+                                                        <td>Long Sleeves</td>
+                                                        <td>80$</td>
+                                                        <td>10%</td>
+                                                        <td>Clothes</td>
+                                                        <td>
+                                                        <a href ="" class='btn btn-sm btn-primary'><i class='la la-pencil'>Edit</i></a>
+                                                        <a href ="" class='btn btn-sm btn-success'><i class='la la-eye'>Details</i></a>
+                                                        <a href ="" class='btn btn-sm btn-danger'><i class='la la-trash-o'>Delete</i></a>
+                                                        
+                                                        </td>
                                                     </tr>
                                                     </tbody>
                                                 </table>
@@ -44,4 +50,12 @@
                 </div>
             </section>
         </div>
+    @endsection
+
+    @section('scripts')
+    <script>
+        $(document).ready( function () {
+        $('#datatable').DataTable();
+        });
+    </script>
     @endsection
