1a58727b7a9cb4acce3487e208b11ac2d33b26fb
Yaya143 <kongsreya143@gmail.com>
2020-09-25 01:03:37 +0700

    update manage_promotion

--- resources/views/Backend/manage-promotion/create.blade.php
+++ resources/views/Backend/manage-promotion/create.blade.php
@@ -10,14 +10,49 @@
                     </div>
                     <div class="card-content collpase show">
                         <div class="card-body">
-                            <form class="form form-horizontal" action="createcategory.php" novalidate method="post">
+                            <form class="form form-horizontal" action="index.blade.php" novalidate method="post">
                                 <div class="form-body">
-                                    <!--                                <h4 class="form-section"><i class="ft-user"></i> Personal Info</h4>-->
+
+                                    <div class="form-group row">
+                                        <label class="col-md-3 label-control" for="projectinput1">Product Name: </label>
+                                        <div class="col-md-5">
+                                            <input type="text" id="projectinput1" class="form-control" placeholder="product name" name="product_name">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-md-3 label-control" for="projectinput1">Category: </label>
+                                        <select class="custom-select col-md-2" id="" style="margin-left:15px;" name="category_id">
+                                            <option value=""></option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-md-3 label-control" for="projectinput1">Brand: </label>
+                                        <select class="custom-select col-md-2" id="" style="margin-left:15px;" name="brand_id">
+                                            <option value=""></option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-md-3 label-control" for="projectinput1">Price: </label>
+                                        <div class="col-md-5">
+                                            <input type="text" id="projectinput1" class="form-control" placeholder=" price" name="price">
+                                        </div>
+                                    </div>
+
                                     <div class="form-group row">
                                         <label class="col-md-3 label-control" for="projectinput1">Category ID: </label>
                                         <div class="col-md-5">
                                             <input type="text" id="projectinput1" class="form-control" placeholder="category id" name="category_id">
+                                        </div>
+                                    </div>
 
+                                    <div class="form-group row">
+
+                                        <label class="col-md-3 label-control" for="projectinput1">Discount: </label>
+                                        <div class="col-md-5">
+                                            <input type="text" id="projectinput1" class="form-control" placeholder=" discount" name="discount">
                                         </div>
                                     </div>
 
@@ -28,19 +63,59 @@
 
                                         </div>
                                     </div>
+
                                     <div class="form-group row">
                                         <label class="col-md-3 label-control" for="projectinput1">Description</label>
                                         <div class="col-md-5">
                                             <textarea name="description" id="projectinput1" cols="70" rows="4"></textarea>
                                         </div>
                                     </div>
+
+
+                                    <div class="form-group row">
+                                        <label class="col-md-3 label-control" for="projectinput1">Image: </label>
+                                        <div class="col-md-5">
+                                            <input type="file" id="projectinput1" class="custom-file-input" placeholder=" image" name="image">
+                                            <label class = "custom-file-label">Choose file</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-md-3 label-control" for="projectinput1">Size: </label>
+                                        <select class="custom-select col-md-2" id="" style="margin-left:15px;" name="size_id">
+                                            <option value=""></option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-md-3 label-control" for="projectinput1">Company Name: </label>
+                                        <div class="col-md-5">
+                                            <input type="text" id="projectinput1" class="form-control" placeholder=" company name" name="company_name">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-md-3 label-control" for="projectinput1">Location: </label>
+                                        <div class="col-md-5">
+                                            <input type="text" id="projectinput1" class="form-control" placeholder=" location" name="location">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-md-3 label-control" for="projectinput1">Phone: </label>
+                                        <div class="col-md-5">
+                                            <input type="text" id="projectinput1" class="form-control" placeholder=" phone" name="phone">
+                                        </div>
+                                    </div>
+
                                 </div>
                                 <div class="form-actions">
-                                    <button type="button" class="btn btn-warning mr-1">
-                                        <a href="{{url('/category')}}"><i class="ft-x"></i> Cancel</a>
+                                    <button type="button" class="btn btn-danger">
+                                        <a href="{{url('/product')}}"><i class="la la-close"></i> Cancel</a>
+
                                     </button>
                                     <button type="submit" name="create" class="btn btn-primary">
-                                        <i class="la la-check-square-o"></i> Create
+                                        <a href="{{url('/product')}}"><i class="la la-check-square-o"></i> Create</a>
                                     </button>
                                 </div>
                             </form>
